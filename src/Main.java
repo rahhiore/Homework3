3acd3e249bba38ab3098445351df49169230c003
Pavel <hareharrionh@gmail.com>
2023-03-26 16:00:54 +0300

    Half commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
+        task8();
     }
 
     public static void task1() {
@@ -59,4 +62,50 @@ public class Main {
         System.out.println("В школе, где " + classes + " классов, нужно " + (whitePaint * classes) +
                 " банок белой краски и " + (brownPaint * classes) + " банок коричневой краски");
     }
+    public static void task6() {
+        int bananas = 5;
+        int milks = 2;
+        int iceCream = 2;
+        int eggs = 4;
+        int totalGrams = bananas * 100 + milks * 105 + iceCream * 100 + eggs * 70;
+        double totalKilograms = totalGrams;
+        totalKilograms /= 1000;
+        System.out.println("Всего в граммах необходимо " + totalGrams + " грамм, в килграммах это, "
+                + totalKilograms + " килограмм");
+
+    }
+
+    public static void daysReturner(int weight, int days) {
+        System.out.println("При худение по " + weight + " грамм в день, необходимо " + days +
+                " дней.");
+    }
+    static void task7() {
+        int weightKg = 7;
+        int minusWeight1 = 250;
+        int minusWeight2 = 500;
+        int weightG = weightKg * 1000;
+        int neededDays1 = weightG / minusWeight1;
+        int neededDays2 = weightG / minusWeight2;
+        int averageDays = (neededDays1 + neededDays2) / 2;
+        int averageWeights = (minusWeight1 + minusWeight2) / 2;
+        daysReturner(minusWeight1, neededDays1);
+        daysReturner(minusWeight2, neededDays2);
+        System.out.println("Средним показателем будет являтся " + averageWeights + " грамм и " + averageDays + " день.");
+    }
+    public static void getUpSalary(String name, int salary) {
+        int newSalary = (int) (salary * 1.1);
+        int yearSalaryBefore = salary * 12;
+        int yearSalaryAfter = newSalary * 12;
+        int difference = yearSalaryAfter - yearSalaryBefore;
+        System.out.println(name + " теперь получает " + newSalary + " рублей. Годовой доход вырос на "
+                + difference + " рублей");
+    }
+    static void task8() {
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKristina = 76230;
+        getUpSalary("Маша", salaryMasha);
+        getUpSalary("Денис", salaryDenis);
+        getUpSalary("Кристина", salaryKristina);
+     }
     }
